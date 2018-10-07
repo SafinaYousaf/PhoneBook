63206f9a813924b17a926d9b427cf80d8e8502bd
SafinaYousaf <41517325+SafinaYousaf@users.noreply.github.com>
2018-10-07 14:47:22 +0500

    This is mini phonebook

---
+++ MiniPhoneBook/MiniPhoneBook/Models/ASPNetUsers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiniPhoneBook.Models
+{
+    public class ASPNetUsers
+    {
+        [Key]
+        public string UserID { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string UserName { get; set;}
+        [Required(ErrorMessage = "Email is required")]
+        public string EmailID { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
